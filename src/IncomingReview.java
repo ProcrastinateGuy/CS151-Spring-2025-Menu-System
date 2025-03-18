1ef07f2b70de2194118d0eda599cde33eabf8b5e
Kanan <kananibadzade@gmail.com>
2025-03-18 16:27:20 -0700

    review classes

--- src/IncomingReview.java
+++ src/IncomingReview.java
@@ -1,2 +1,35 @@
-public class IncomingReview {
+
+import java.time.LocalDateTime;
+
+public class IncomingReview extends Review {
+
+    protected boolean isFlaggedForModeration;
+
+    protected IncomingReview(String reviewId, CustomerAccount customerAccount, double rating, String reviewText, LocalDateTime reviewDate, boolean isFlaggedForModeration) {
+        super(reviewId, customerAccount, rating, reviewText, reviewDate);
+        this.isFlaggedForModeration = isFlaggedForModeration;
+    }
+
+    public boolean isFlaggedForModeration() {
+        return isFlaggedForModeration;
+    }
+
+    public void setFlaggedForModeration(boolean flaggedForModeration) {
+        isFlaggedForModeration = flaggedForModeration;
+    }
+
+    public void flagReviewForModeration() {
+        this.isFlaggedForModeration = true;
+        System.out.println("Incoming review " + reviewId + " has been flagged for moderation by " + customerAccount.getCustomerName());
+    }
+
+    @Override
+    public void writeReview() {
+        System.out.println("----- Incoming Review -----");
+        System.out.println("By: " + customerAccount.getCustomerName() + " on " + reviewDate);
+        System.out.println("Rating: " + rating + " (Flagged: " + isFlaggedForModeration + ")");
+        System.out.println("Comment: " + reviewText);
+        System.out.println("----------------------------");
+    }
+
 }
