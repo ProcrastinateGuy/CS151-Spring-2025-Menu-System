11a22db53e5eed8ce8eef0aee8a3b081a91b3ed0
ProcrastinateGuy <ching91202@gmail.com>
2025-03-18 17:05:31 -0700

    1. changed interface method 2. implemented generateID logic for 3 manager classes

--- src/ManagerInterface.java
+++ src/ManagerInterface.java
@@ -1,11 +1,12 @@
+//manager interface
 public interface ManagerInterface <T> {
     // this interface defines what a "xyz-manager" class needs to implement
     // CustomerAccount class is considered one of the class in this category
 
     // ID generation method for the object it's managing
     // allows multiple Strings as parameter
-    String generateID(String source1, String source2, String source3);
-    String getID();
+    String generateID();
+    String getID( T itemToCheck);
 
     // this method has a generic return type
     T getMember(String memberID);
